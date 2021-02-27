a9d666739a84ac882afa465f7c81397d790fa139
Selina0w0 <Selina0w0@users.noreply.github.com>
2021-02-27 16:32:53 +0800

    hw2 halfway

---
+++ Flix/Flix/MovieDetailsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MovieDetailsViewController.swift
+//  Flix
+//
+//  Created by Selina Fu on 2021/2/27.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    var movie: [String:Any]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
